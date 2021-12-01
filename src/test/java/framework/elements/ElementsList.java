c275a75b5421b395fbfcd41e7321501e8117cd87
Vladislav.Saroka <Vladislav.Saroka@itechart-group.com>
2021-12-01 17:15:46 +0300

    last fixes

---
+++ src/test/java/framework/elements/ElementsList.java
@@ -0,0 +1,9 @@
+package framework.elements;
+
+import org.openqa.selenium.By;
+
+public class ElementsList extends BaseElement{
+    public ElementsList(By locator) {
+        super(locator);
+    }
+}
